0ca11cd2aea913c0161c31c2aeb8650e3ae6037b
Miropolsky <Miropolskysk@yandex.ru>
2024-07-03 17:57:00 +0500

    1

---
+++ shop/src/router/Router.tsx
@@ -0,0 +1,25 @@
+import { App } from "@/components/App";
+import { LazyShop } from "@/pages/shop/Shop.lazy";
+import { Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/shop",
+    element: <App/>,
+    children: [
+      {
+        path: '/shop/main',
+        element: <Suspense fallback={'Loading'}><LazyShop/></Suspense >
+      },
+      {
+        path: '/shop/second',
+        element: <Suspense fallback={'Loading'}><div style={{color: 'red'}}>second</div></Suspense >
+      }
+    ]
+  },
+]
+
+export const router = createBrowserRouter(routes);
+
+export default routes;
